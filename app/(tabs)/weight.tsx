8918e63e363cefde5dac6491b0e1cc41520633fd
William Benarto <w.benarto@gmail.com>
2024-12-29 12:10:26 -0800

    reconfig react native expo

---
+++ app/(tabs)/weight.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+
+const Weight = () => {
+    return (
+
+        <View>
+            <Text>
+                Insert Weight Goal
+            </Text>
+        </View>
+    )
+}
+
+export default Weight;
