3a947825455dd88d964c9a597d6925fe900309cd
William Benarto <w.benarto@gmail.com>
2025-01-05 18:09:51 -0800

    welcome page

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from "react-native-swiper";
+import { useRef, useState } from 'react';
+import { onboardingPages } from '@/constants';
+import CustomButton from '@/components/CustomButton'
 
 const Onboarding = () => {
+    const swiperRef = useRef<Swiper>(null)
+    const [activeIndex, setActiveIndex] = useState(0) 
     return (
-        <SafeAreaView className='bg-red-200 h-full items-center justify-between'>
-            <Text className="text-4xl  ">Onboarding hello Page</Text>
+        <SafeAreaView className='bg-[#262135] h-full items-center justify-between'>
+            <TouchableOpacity 
+            onPress={()=>{
+                router.replace('/(auth)/sign-up')
+            }}
+            className='w-full flex justify-end items-end p-5'>
+                <Text className="text-white text-md font-JakartaBold ">Skip</Text>
+            </TouchableOpacity>
+
+            <Swiper ref={swiperRef} loop={false} 
+            dot={<View className='w-[32px] h-[4px] mx-1 bg-[#E2E8F0]' />}
+            activeDot={<View className='w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full' />} 
+            onIndexChanged={(index) => setActiveIndex(index)}
+            >
+                {onboardingPages.map((e) => (
+                    <View key={e.id} className='flex items-center justify-center p-5 mt-[20%]'>
+                        <Image source={e.image}
+                            className='w-full h-[300px]'
+                            resizeMode='contain'
+                        /> 
+                        <View className='flex flex-row items-center justify-center w-full mt-10'>
+                            <Text className='text-white text-3xl font-bold mx-10 text-center'>{e.title}</Text>
+                        </View>
+                        
+                        <Text className='text-white text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3'>{e.description}</Text>
+                    </View>
+                ))}
+            </Swiper>
+
+            <CustomButton 
+            title='Next'
+            className='w-11/12 mt-10'
+            />
+            
         </SafeAreaView>
     )
 }
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+import Icon from '@/assets/images/icon.png'
+export const images = {
+    Icon
+}
+
+export const icons = {
+    Icon
+}
+
+export const onboardingPages = [
+    {
+        id: 1,
+        title: 'Welcome to Fortia',
+        description: 'Your Fitness Journey Starts Here.',
+        image: images.Icon,
+    },
+    {
+        id: 2,
+        title: 'Track and Achieve Your Goals ',
+        description: 'Set your fitness goals, track progress, and celebrate your achievements—all in one place.',
+        image: images.Icon,
+    }
+]
