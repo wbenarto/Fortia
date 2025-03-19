96ce22631a09fe9afab3692eae554d7caa20f5e7
William Benarto <williambenarto@Williams-MacBook-Pro.local>
2025-03-18 22:56:59 -0700

    account information page

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -12,6 +12,11 @@ const Profile = () => {
         signOut()
         router.replace("/(auth)/sign-in")
     }
+
+    const handleAccountInfo = () => {
+        router.push("/accountInformation")
+    }
+
     return (
         <View className='bg-[#262135] w-full h-full text-white '>
             
@@ -30,17 +35,17 @@ const Profile = () => {
                     App Settings
                 </Text>
                 <View className='w-full  bg-[#494358] rounded-[30px]'>
-                    <View className=' h-20 w-[90%] mx-auto flex flex-row justify-between px-4 items-center border-b-2 border-gray-400 '>
-                        <Text className='font-JakartaSemiBold text-white' >Account Informations</Text>
-                        <Text className='font-JakartaSemiBold text-white'>></Text>
-                    </View>
+                    <TouchableOpacity onPress={handleAccountInfo} className=' h-20 w-[90%] mx-auto flex flex-row justify-between px-4 items-center border-b-2 border-gray-400 '>
+                        <Text className='font-JakartaSemiBold text-white' >Account Information</Text>
+                        <Text className='font-JakartaSemiBold text-white'>{'>'}</Text>
+                    </TouchableOpacity>
                     <View className=' h-20 w-[90%] mx-auto flex flex-row justify-between px-4 items-center border-b-2 border-gray-400 '>
                         <Text className='font-JakartaSemiBold text-white' >Subscriptions</Text>
-                        <Text className='font-JakartaSemiBold text-white'>></Text>
+                        <Text className='font-JakartaSemiBold text-white'>{'>'}</Text>
                     </View>
                     <View className=' h-20 w-[90%] mx-auto flex flex-row justify-between px-4 items-center '>
                         <Text className='font-JakartaSemiBold text-white' >Terms of Service</Text>
-                        <Text className='font-JakartaSemiBold text-white'>></Text>
+                        <Text className='font-JakartaSemiBold text-white'>{'>'}</Text>
                     </View>
 
                 </View>
---
+++ app/(root)/accountInformation.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+
+const AccountInformationScreen = () => {
+    return (
+        <View className="flex-1 bg-[#262135]">
+        </View>
+    );
+};
+
+export default AccountInformationScreen;
